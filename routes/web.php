802f43e1e1a932f68e94d102f76e30661c24cbd4
ibrahim hasanain <ibraheemhasanien18@gmail.com>
2023-08-01 20:30:10 +0300

    create auth

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Dashboard\HomeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
+});
+
+Auth::routes();
+
+Route::group(['middleware' => 'auth'], function (){
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
 });
